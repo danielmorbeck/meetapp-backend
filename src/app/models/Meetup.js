0e4845b7ee2a56431253f52474fb896efdeb6a57
Daniel Morbeck <danielmorbeck@gmail.com>
2019-07-09 16:09:37 -0300

    Meetup migration and model created

---
+++ src/app/models/Meetup.js
@@ -0,0 +1,26 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Meetup extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        title: Sequelize.STRING,
+        description: Sequelize.STRING,
+        location: Sequelize.STRING,
+        date: Sequelize.DATE,
+        path: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'organizer' });
+  }
+}
+
+export default Meetup;
